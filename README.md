c3a1f7dfde14c62dedc88113fbf8fada310ea8d1
Matan Mates <matan@matan.me>
2019-12-06 23:47:48 +0000

    Updated configuration

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# configuration
+Manual Install
+===============
+
+* [nvim](https://github.com/neovim/neovim/wiki/Installing-Neovim)
+* [vim-plug](https://github.com/junegunn/vim-plug)
+
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt install nvim
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/local/bin/nvim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +'PlugInstall --sync' +qa
