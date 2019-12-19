fe63d7bc0a0a75bc53f9b695d3da6ba100e96ad7
Matan Mates <matan@matan.me>
2019-12-19 21:08:42 +0000

    added pipenv

--- install.sh
+++ install.sh
@@ -20,6 +20,9 @@ pyenv install 3.7-dev
 pyenv shell 2.7-dev
 pip install -r config/pip-27.txt
 
+pyenv shell 3.7-dev
+pip install -r config/pip-37.txt
+
 pyenv global 3.8-dev
 pyenv init -
 pip install --upgrade pip
@@ -27,9 +30,12 @@ pip install -r config/pip.txt
 python3 -m pip install --upgrade git+https://github.com/Gallopsled/pwntools.git@dev3
 
 
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x nvim.appimage
+chmod u+x kubectl
 sudo mv nvim.appimage /usr/local/bin/nvim
+sudo mv kubectl /usr/local/bin/kubectl
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +'PlugInstall --sync' +qa
 
