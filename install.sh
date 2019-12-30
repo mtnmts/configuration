7c4d1091bc950b1b2291e1abbafc7542368c5a24
Matan Mates <matan@matan.me>
2019-12-30 14:00:26 +0100

    Removed install.sh temporarily until it's fixed

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bash mkdir -p $LOGS
-bash scripts/apt.sh > $LOGS/apt.log 2>&1
-bash scripts/copy_config.sh > $LOGS/copy_config.log 2>&1
-bash scripts/pyenv.sh > $LOGS/pyenv.log 2>&1
-bash scripts/python_setup.sh > $LOGS/python_setup.log 2>&1
-bash scripts/rust.sh > $LOGS/rust.log 2>&1
-bash scripts/misc.sh > $LOGS/misc.log 2>&1 
