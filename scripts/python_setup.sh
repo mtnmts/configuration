cb23a83e9a310934e4be27b2d951a966c50f5cd6
Matan Mates <matan@matan.me>
2019-12-30 14:02:59 +0100

    update pyenv stuff

--- scripts/python_setup.sh
+++ scripts/python_setup.sh
@@ -3,13 +3,13 @@ export PYENV_ROOT=$HOME/.pyenv
 export PATH=$PYENV_ROOT/bin:$PATH
 PYENV=$PYENV_ROOT/bin/pyenv
 
-$PYENV init -
-$PYENV virtualenv-init -
+eval "$($PYENV init -)" 
+eval "$($PYENV virtualenv-init -)"
 
 # 3.8-dev (Global)
 $PYENV install 3.8-dev
 $PYENV global 3.8-dev
-$PYENV init -
+eval "$($PYENV init -)" 
 $PYENV exec pip install --upgrade pip
 $PYENV exec pip install -r config/pip.txt
 $PYENV exec pip install --upgrade git+https://github.com/Gallopsled/pwntools.git@dev3
@@ -18,11 +18,11 @@ $PYENV exec pip install --upgrade git+https://github.com/Gallopsled/pwntools.git
 $PYENV install 2.7-dev
 $PYENV global 2.7-dev
 $PYENV shell 2.7-dev
-$PYENV init -
+eval "$($PYENV init -)" 
 $PYENV exec pip install -r config/pip-27.txt
 
 $PYENV global 3.8-dev
-$PYENV init -
+eval "$($PYENV init -)" 
 $PYENV shell 3.8-dev
 
 pip3 install pynvim
